64d73184604602cb7cd62d88fab4c5b8bdc99a49
Maria Gabriela M Oliveira <magabi1610@gmail.com>
2024-11-27 08:29:51 -0300

    Envio da atividade II ao repositorio com o HATEOAS implementado

---
+++ automanager/src/main/java/com/autobots/automanager/entidades/Documento.java
@@ -0,0 +1,26 @@
+package com.autobots.automanager.entidades;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.RepresentationModel;
+
+@Data
+@Entity
+public class Documento extends RepresentationModel<Documento> {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column
+	private String tipo;
+	@Column(unique = true)
+	private String numero;
+	public void addLink(Link link) {
+		this.add(link);
+	}
+}
